213a8c33a6a9207f5e1355597d28006ed1349a91
Jaehwa Noh <shwoghk14@gmail.com>
2023-11-04 18:29:59 +0900

    Create Basic skeleton.

--- Compose-Art-Space-App/app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ Compose-Art-Space-App/app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -3,10 +3,10 @@ package com.example.artspaceapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyGallery()
                 }
             }
         }
@@ -30,17 +30,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyGallery(modifier: Modifier = Modifier) {
+    Column {
+        MyPicture()
+        PictureArtistAndName()
+        NextAndPreviousButton()
+    }
+
+}
+
+@Composable
+fun MyPicture(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun PictureArtistAndName(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun NextAndPreviousButton() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArtSpaceAppTheme {
-        Greeting("Android")
+        MyGallery()
     }
 }
