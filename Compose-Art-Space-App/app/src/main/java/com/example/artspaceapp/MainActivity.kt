49c4c6c156b2964082b403b5d658f94d7d1c5d82
Jaehwa Noh <shwoghk14@gmail.com>
2023-11-04 19:24:57 +0900

    Add artist, name, year text.

--- Compose-Art-Space-App/app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ Compose-Art-Space-App/app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -4,21 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspaceapp.ui.theme.ArtSpaceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -70,8 +78,30 @@ fun MyPicture(modifier: Modifier = Modifier) {
 
 @Composable
 fun PictureArtistAndName(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(20.dp)
+            .background(Color.LightGray)
+            .padding(20.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.puppy),
+            fontSize = 28.sp
+        )
 
+        Row {
+            Text(
+                text = stringResource(id = R.string.puppy_artist),
+                fontWeight = FontWeight.Bold,
+            )
+            Text(text = " (" + stringResource(id = R.string.puppy_year) + ")")
+        }
+
+    }
 }
+
 
 @Composable
 fun NextAndPreviousButton(
