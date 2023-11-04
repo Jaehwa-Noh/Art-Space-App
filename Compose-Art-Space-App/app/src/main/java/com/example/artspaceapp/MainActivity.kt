b88977206dedf6efd7a73851ae60b061e50cdc75
Jaehwa Noh <shwoghk14@gmail.com>
2023-11-04 18:44:20 +0900

    Apply arrangement and alignment.

--- Compose-Art-Space-App/app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ Compose-Art-Space-App/app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.artspaceapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.artspaceapp.ui.theme.ArtSpaceAppTheme
@@ -31,12 +33,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyGallery(modifier: Modifier = Modifier) {
-    Column {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         MyPicture()
         PictureArtistAndName()
         NextAndPreviousButton()
     }
-
 }
 
 @Composable
