587044424cd76f32777b2ccb4ca3f770b0410e0a
shwoghk14 <shwoghk14@gmail.com>
2023-11-06 23:02:14 +0900

    Project init.

---
+++ Swift-Art-Space-App/ArtSpaceApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArtSpaceApp
+//
+//  Created by Jaehwa Noh on 11/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
